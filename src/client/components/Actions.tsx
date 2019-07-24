2685f9e797f1f29db8cdb04d91d33c061fec41e3
Rob Hofmann <rob.hofmann@HOFMANN.int>
2019-07-24 18:59:31 +0200

    layout

--- src/client/components/Actions.tsx
+++ src/client/components/Actions.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { ClientConnectedEvent } from "../../model/Events";
-import { Button } from "bloomer";
+import { Button, Table, Tile, Box, Title } from "bloomer";
 
 import * as styles from "./Actions.scss";
 
@@ -18,6 +18,100 @@ const Actions: React.FunctionComponent<Props> = (props) =>
         <Button className={styles.button} onClick={props.onStartArchiverButtonClick}>Start Archiver</Button>
         <Button className={styles.button} onClick={props.onStartUploaderButtonClick}>Start Uploader</Button>
         <Button className={styles.button} onClick={props.onOptionsButtonClick}>Options</Button>
+
+        <Tile isAncestor>
+            <Tile isSize={4} isVertical isParent>
+                <Tile isChild render={
+                    props => {
+                        return (<Box {...props}>
+                            <Title>Saved Clips</Title>
+                            <Table isBordered isStriped isNarrow>
+                                <thead>
+                                    <tr>
+                                        <th>Timestamp</th>
+                                        <th>Filename</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>2019-07-24 18:54:00</td>
+                                        <td>2019-07-22_20-48-23-right_repeater.mp4</td>
+                                    </tr>
+                                    <tr>
+                                        <td>2019-07-24 18:54:00</td>
+                                        <td>2019-07-22_20-48-23-front.mp4</td>
+                                    </tr>
+                                    <tr>
+                                        <td>2019-07-24 18:54:00</td>
+                                        <td>2019-07-22_20-48-23-left_repeater.mp4</td>
+                                    </tr>
+                                </tbody>
+                            </Table>
+                        </Box>);
+                    }
+                } />
+                <Tile isChild render={
+                    props => {
+                        return (<Box {...props}>
+                            <Title>Recent Clips</Title>
+                            <Table isBordered isStriped isNarrow>
+                                <thead>
+                                    <tr>
+                                        <th>Timestamp</th>
+                                        <th>Filename</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>2019-07-24 18:54:00</td>
+                                        <td>2019-07-22_20-48-23-right_repeater.mp4</td>
+                                    </tr>
+                                    <tr>
+                                        <td>2019-07-24 18:54:00</td>
+                                        <td>2019-07-22_20-48-23-front.mp4</td>
+                                    </tr>
+                                    <tr>
+                                        <td>2019-07-24 18:54:00</td>
+                                        <td>2019-07-22_20-48-23-left_repeater.mp4</td>
+                                    </tr>
+                                </tbody>
+                            </Table>
+                        </Box>);
+                    }
+                } />
+            </Tile>
+            <Tile isParent>
+                <Tile isChild render={
+                    props => {
+                        return (<Box {...props}>
+                            <Title size={1}>Archived Clips</Title>
+                            <Table isBordered isStriped isNarrow>
+                                <thead>
+                                    <tr>
+                                        <th>Timestamp</th>
+                                        <th>Filename</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>2019-07-24 18:54:00</td>
+                                        <td>2019-07-22_20-48-23-right_repeater.mp4</td>
+                                    </tr>
+                                    <tr>
+                                        <td>2019-07-24 18:54:00</td>
+                                        <td>2019-07-22_20-48-23-front.mp4</td>
+                                    </tr>
+                                    <tr>
+                                        <td>2019-07-24 18:54:00</td>
+                                        <td>2019-07-22_20-48-23-left_repeater.mp4</td>
+                                    </tr>
+                                </tbody>
+                            </Table>
+                        </Box>);
+                    }
+                } />
+            </Tile>
+        </Tile>
     </>;
 
 export default Actions;
--- src/client/pages/IndexPage.tsx
+++ src/client/pages/IndexPage.tsx
@@ -11,8 +11,11 @@ const IndexPage: React.FunctionComponent = () =>
         <Column>
             <Box>
                 <Title size={4}>Status</Title>
-                <p>Something: Something</p>
-                <p>Something: something2</p>
+                <p>Last archive: 2019-07-23 18:54:00</p>
+                <p>Last upload: 2019-07-23 18:52:12</p>
+                <p>Disk: 24GB free, 40GB used, 64GB total</p>
+                <p>28GB in Saved Clips</p>
+                <p>12GB in Recent Clips</p>
             </Box>
         </Column>
     </Columns>;
