c2f4b7048e6a79d5ceeff01c34e6576a00a1d45b
Michiel Lowijs <michiel.lowijs@sycade.com>
2019-07-24 19:04:53 +0200

    remove render stuff

--- src/client/components/Actions.tsx
+++ src/client/components/Actions.tsx
@@ -23,95 +23,89 @@ const Actions: React.FunctionComponent<Props> = (props) =>
 
         <Tile isAncestor>
             <Tile isSize={4} isVertical isParent>
-                <Tile isChild render={
-                    props => {
-                        return (<Box {...props}>
-                            <Title>Saved Clips</Title>
-                            <Table isBordered isStriped isNarrow>
-                                <thead>
-                                    <tr>
-                                        <th>Timestamp</th>
-                                        <th>Filename</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td>2019-07-24 18:54:00</td>
-                                        <td>2019-07-22_20-48-23-right_repeater.mp4</td>
-                                    </tr>
-                                    <tr>
-                                        <td>2019-07-24 18:54:00</td>
-                                        <td>2019-07-22_20-48-23-front.mp4</td>
-                                    </tr>
-                                    <tr>
-                                        <td>2019-07-24 18:54:00</td>
-                                        <td>2019-07-22_20-48-23-left_repeater.mp4</td>
-                                    </tr>
-                                </tbody>
-                            </Table>
-                        </Box>);
-                    }
-                } />
-                <Tile isChild render={
-                    props => {
-                        return (<Box {...props}>
-                            <Title>Recent Clips</Title>
-                            <Table isBordered isStriped isNarrow>
-                                <thead>
-                                    <tr>
-                                        <th>Timestamp</th>
-                                        <th>Filename</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td>2019-07-24 18:54:00</td>
-                                        <td>2019-07-22_20-48-23-right_repeater.mp4</td>
-                                    </tr>
-                                    <tr>
-                                        <td>2019-07-24 18:54:00</td>
-                                        <td>2019-07-22_20-48-23-front.mp4</td>
-                                    </tr>
-                                    <tr>
-                                        <td>2019-07-24 18:54:00</td>
-                                        <td>2019-07-22_20-48-23-left_repeater.mp4</td>
-                                    </tr>
-                                </tbody>
-                            </Table>
-                        </Box>);
-                    }
-                } />
+                <Tile isChild>
+                    <Box {...props}>
+                        <Title>Saved Clips</Title>
+                        <Table isBordered isStriped isNarrow>
+                            <thead>
+                                <tr>
+                                    <th>Timestamp</th>
+                                    <th>Filename</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>2019-07-24 18:54:00</td>
+                                    <td>2019-07-22_20-48-23-right_repeater.mp4</td>
+                                </tr>
+                                <tr>
+                                    <td>2019-07-24 18:54:00</td>
+                                    <td>2019-07-22_20-48-23-front.mp4</td>
+                                </tr>
+                                <tr>
+                                    <td>2019-07-24 18:54:00</td>
+                                    <td>2019-07-22_20-48-23-left_repeater.mp4</td>
+                                </tr>
+                            </tbody>
+                        </Table>
+                    </Box>
+                </Tile>
+                <Tile isChild>
+                    <Box {...props}>
+                        <Title>Recent Clips</Title>
+                        <Table isBordered isStriped isNarrow>
+                            <thead>
+                                <tr>
+                                    <th>Timestamp</th>
+                                    <th>Filename</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>2019-07-24 18:54:00</td>
+                                    <td>2019-07-22_20-48-23-right_repeater.mp4</td>
+                                </tr>
+                                <tr>
+                                    <td>2019-07-24 18:54:00</td>
+                                    <td>2019-07-22_20-48-23-front.mp4</td>
+                                </tr>
+                                <tr>
+                                    <td>2019-07-24 18:54:00</td>
+                                    <td>2019-07-22_20-48-23-left_repeater.mp4</td>
+                                </tr>
+                            </tbody>
+                        </Table>
+                    </Box>
+                </Tile>
             </Tile>
             <Tile isParent>
-                <Tile isChild render={
-                    props => {
-                        return (<Box {...props}>
-                            <Title size={1}>Archived Clips</Title>
-                            <Table isBordered isStriped isNarrow>
-                                <thead>
-                                    <tr>
-                                        <th>Timestamp</th>
-                                        <th>Filename</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td>2019-07-24 18:54:00</td>
-                                        <td>2019-07-22_20-48-23-right_repeater.mp4</td>
-                                    </tr>
-                                    <tr>
-                                        <td>2019-07-24 18:54:00</td>
-                                        <td>2019-07-22_20-48-23-front.mp4</td>
-                                    </tr>
-                                    <tr>
-                                        <td>2019-07-24 18:54:00</td>
-                                        <td>2019-07-22_20-48-23-left_repeater.mp4</td>
-                                    </tr>
-                                </tbody>
-                            </Table>
-                        </Box>);
-                    }
-                } />
+                <Tile isChild>
+                    <Box {...props}>
+                        <Title size={1}>Archived Clips</Title>
+                        <Table isBordered isStriped isNarrow>
+                            <thead>
+                                <tr>
+                                    <th>Timestamp</th>
+                                    <th>Filename</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>2019-07-24 18:54:00</td>
+                                    <td>2019-07-22_20-48-23-right_repeater.mp4</td>
+                                </tr>
+                                <tr>
+                                    <td>2019-07-24 18:54:00</td>
+                                    <td>2019-07-22_20-48-23-front.mp4</td>
+                                </tr>
+                                <tr>
+                                    <td>2019-07-24 18:54:00</td>
+                                    <td>2019-07-22_20-48-23-left_repeater.mp4</td>
+                                </tr>
+                            </tbody>
+                        </Table>
+                    </Box>
+                </Tile>
             </Tile>
         </Tile>
     </>;
