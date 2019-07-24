570d2b8b3f3cc01f5feb402eb95e88f8b005c678
Michiel Lowijs <michiel.lowijs@sycade.com>
2019-07-24 23:33:07 +0200

    Fix styles

--- src/client/components/Actions.tsx
+++ src/client/components/Actions.tsx
@@ -1,6 +1,7 @@
+import * as styles from "./Actions.scss";
 import * as React from "react";
 import { ClientConnectedEvent } from "../../model/Events";
-import { Button, Title, Box, Tile } from "bloomer";
+import { Button, Title, Box } from "bloomer";
 
 interface Props {
     event: ClientConnectedEvent;
@@ -11,15 +12,13 @@ interface Props {
 }
 
 const Actions: React.FunctionComponent<Props> = (props) =>
-    <Tile isChild render={(tileProps: any) =>
-        <Box {...tileProps}>
-            <Title isSize={4}>Actions</Title>
+    <Box className={styles.Actions}>
+        <Title isSize={4}>Actions</Title>
 
-            <Button onClick={props.onSyncRecentsButtonClick}>Sync Recents</Button>
-            <Button onClick={props.onStartArchiverButtonClick}>Start Archiver</Button>
-            <Button onClick={props.onStartUploaderButtonClick}>Start Uploader</Button>
-            <Button onClick={props.onOptionsButtonClick}>Options</Button>
-        </Box>}
-    />;
+        <Button onClick={props.onSyncRecentsButtonClick}>Sync Recents</Button>
+        <Button onClick={props.onStartArchiverButtonClick}>Start Archiver</Button>
+        <Button onClick={props.onStartUploaderButtonClick}>Start Uploader</Button>
+        <Button onClick={props.onOptionsButtonClick}>Options</Button>
+    </Box>;
 
 export default Actions;
--- src/client/components/Status.tsx
+++ src/client/components/Status.tsx
@@ -1,17 +1,15 @@
 import * as React from "react";
-import { Title, Box, Tile } from "bloomer";
+import { Title, Box } from "bloomer";
 
 const Status: React.FunctionComponent = () =>
-    <Tile isChild render={(tileProps: any) =>
-        <Box {...tileProps}>
+    <Box>
 
-            <Title size={4}>Status</Title>
-            <p>Last archive: 2019-07-23 18:54:00</p>
-            <p>Last upload: 2019-07-23 18:52:12</p>
-            <p>Disk: 24GB free, 40GB used, 64GB total</p>
-            <p>28GB in Saved Clips</p>
-            <p>12GB in Recent Clips</p>
-        </Box>
-    } />;
+        <Title size={4}>Status</Title>
+        <p>Last archive: 2019-07-23 18:54:00</p>
+        <p>Last upload: 2019-07-23 18:52:12</p>
+        <p>Disk: 24GB free, 40GB used, 64GB total</p>
+        <p>28GB in Saved Clips</p>
+        <p>12GB in Recent Clips</p>
+    </Box>;
 
 export default Status;
