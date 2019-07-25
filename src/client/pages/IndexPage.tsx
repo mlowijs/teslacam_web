15a0e34bd73b25625b89986082d56607b99c8499
Michiel Lowijs <michiel.lowijs@sycade.com>
2019-07-25 09:57:27 +0200

    filestype

--- src/client/pages/IndexPage.tsx
+++ src/client/pages/IndexPage.tsx
@@ -3,6 +3,7 @@ import ActionsContainer from "../containers/ActionsContainer";
 import StatusContainer from "../containers/StatusContainer";
 import { Container, Tile, Section, Columns, Column } from "bloomer";
 import FilesContainer from "../containers/FilesContainer";
+import { FilesType } from "../../model/Enums";
 
 const IndexPage: React.FunctionComponent = () =>
     <Section>
@@ -18,12 +19,12 @@ const IndexPage: React.FunctionComponent = () =>
             
             <Tile isAncestor>
                 <Tile isSize={6} isVertical isParent>
-                    <FilesContainer title="Saved" />
-                    <FilesContainer title="Recent" />
+                    <FilesContainer filesType={FilesType.SAVED} title="Saved" />
+                    <FilesContainer filesType={FilesType.RECENT} title="Recent" />
                 </Tile>
 
                 <Tile isVertical isParent>
-                    <FilesContainer title="Archive" />
+                    <FilesContainer filesType={FilesType.ARCHIVE} title="Archive" />
                 </Tile>
             </Tile>
         </Container>
