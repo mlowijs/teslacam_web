bea1f2fb3f6dae7dc0a326d5870bd5efd5d24b8c
Michiel Lowijs <michiel.lowijs@sycade.com>
2019-07-28 17:47:31 +0200

    more styling

--- src/client/components/Actions.tsx
+++ src/client/components/Actions.tsx
@@ -11,12 +11,12 @@ interface Props {
 
 const Actions: React.FunctionComponent<Props> = (props) =>
     <Box className={styles.Actions}>
-        <Title className="is-size-3-touch">Actions</Title>
+        <Title className="is-size-2-touch">Actions</Title>
 
-        <Button onClick={props.onSyncRecentsButtonClick}>Sync Recents</Button>
-        <Button onClick={props.onStartArchiverButtonClick}>Start Archiver</Button>
-        <Button onClick={props.onStartUploaderButtonClick}>Start Uploader</Button>
-        <Button onClick={props.onOptionsButtonClick}>Options</Button>
+        <Button className="is-size-2-touch" onClick={props.onSyncRecentsButtonClick}>Sync Recents</Button>
+        <Button className="is-size-2-touch" onClick={props.onStartArchiverButtonClick}>Start Archiver</Button>
+        <Button className="is-size-2-touch" onClick={props.onStartUploaderButtonClick}>Start Uploader</Button>
+        <Button className="is-size-2-touch" onClick={props.onOptionsButtonClick}>Options</Button>
     </Box>;
 
 export default Actions;
