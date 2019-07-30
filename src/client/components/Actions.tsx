19d2e7a8f900cbdc0a3327b08ec1cfa8b45044f8
Michiel Lowijs <michiel.lowijs@sycade.com>
2019-07-30 14:23:42 +0200

    button component

--- src/client/components/Actions.tsx
+++ src/client/components/Actions.tsx
@@ -1,6 +1,7 @@
 import * as styles from "./Actions.scss";
 import * as React from "react";
 import classNames from "classnames";
+import Button from "./Button";
 
 interface Props {
     onSyncRecentsButtonClick: () => void;
@@ -13,10 +14,10 @@ const Actions: React.FunctionComponent<Props> = (props) =>
     <div className={classNames(styles.box, styles.Actions)}>
         <div className={classNames(styles.title, styles.isSize2Touch)}>Actions</div>
 
-        <button className={classNames(styles.button, styles.isSize2Touch)} onClick={props.onSyncRecentsButtonClick}>Sync Recents</button>
-        <button className={classNames(styles.button, styles.isSize2Touch)} onClick={props.onStartArchiverButtonClick}>Start Archiver</button>
-        <button className={classNames(styles.button, styles.isSize2Touch)} onClick={props.onStartUploaderButtonClick}>Start Uploader</button>
-        <button className={classNames(styles.button, styles.isSize2Touch)} onClick={props.onOptionsButtonClick}>Options</button>
+        <Button onClick={props.onSyncRecentsButtonClick}>Sync Recents</Button>
+        <Button onClick={props.onStartArchiverButtonClick}>Start Archiver</Button>
+        <Button onClick={props.onStartUploaderButtonClick}>Start Uploader</Button>
+        <Button onClick={props.onOptionsButtonClick}>Options</Button>
     </div>;
 
 export default Actions;
